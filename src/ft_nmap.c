6949d87850cb2b0f0c3fea5ca0071a9255e4532d
moulin-louis <louis.moulin@outlook.fr>
2024-03-19 16:59:22 +0100

    wipo

--- src/ft_nmap.c
+++ src/ft_nmap.c
@@ -4,8 +4,19 @@
 
 #include "ft_nmap.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
 
-int main(void) {
-  printf("hello world\n");
+
+int main(int ac, char **av) {
+  (void)ac;
+  (void)av;
+  int sck = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+  if (sck == -1) {
+    perror("socket SOCK RAW");
+    return 1;
+  }
+  printf("sck = %d\n", sck);
   return 0;
 }
